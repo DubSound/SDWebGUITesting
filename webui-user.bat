209151ee139c0d7e3c724f01e0d88c019090d586
WTFJezz <jaybentham@googlemail.com>
2023-02-18 18:09:02 +0000

    Style

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--api --cors-allow-origins=https://www.painthua.com
+set COMMANDLINE_ARGS=--ckpt models/Stable-diffusion/vintedois-diff-v0-1.ckpt
 git pull
 call webui.bat
